f4d35c1d82cbde7482ed26188e1810971b371266
hack-c <charles.hack@accenture.com>
2015-05-19 01:05:12 -0400

    new file utils with paste function

---
+++ caspanda/utils.py
@@ -0,0 +1,14 @@
+###################################################
+#################[ Module: Utils ]#################
+###################################################
+"""
+Miscellaneous utilities for caspanda.
+"""
+
+
+def paste(x, sep = ", "):
+    """
+    Custom string formatting function to format (???) output.
+    """
+
+    return str(x).strip("[]").replace("'","").replace(", ", sep)
