ba0081e6f07e4c7b1673621a575c1bc3aace770b
hack-c <charles.hack@accenture.com>
2015-05-19 01:06:16 -0400

    expanded bamboo.py with docstrings and made a few hopefully purely cosmetic style edits

--- caspanda/bamboo.py
+++ caspanda/bamboo.py
@@ -1,148 +1,199 @@
-from cassandra.cluster import Session
-from pandas import DataFrame
-import logging as log
+###################################################
+#################[ Module: Bamboo ]################
+###################################################
+"""
+This module contains the CassandraFrame class, which exposes the main interface between pandas and Cassandra.
+
+Bamboo, like what PANDAS eat. Get it??
+
+CassandraFrame implements synchronous and asynchronous insertion operations, and MultiIndexes output from Cassandra
+in order to support pivot- and melt-like operations.
+"""
+import logging
+import pandas as pd
 from Queue import Queue
+from Queue import Empty
+
+from cassandra.cluster import Session
+from cassandra.utils import paste
 
 
+class CassandraFrame(pd.DataFrame):
+    """
+    Wrapper for pandas.DataFrame.
 
-class CassandraFrame(DataFrame):
-    __prepared_columns__ = None
-    statement_input = None
-    __insert_index = None
+    Implements convenience methods for get and put operations to Cassandra,
+    and handles MultiIndexing of CQL output.
 
-    def __init__(self,  data=None, index=None, columns=None, cql = None, session=None, table=None, dtype=None,
+    Keeps track of column name hierarchy in the self._prepared_columns and self._cql_columns.
+    """
+    statement_input      = None
+    _prepared_columns    = None
+    _insert_index        = None
+
+
+    def __init__(self, data=None, index=None, columns=None, cql=None, session=None, table=None, dtype=None,
                  copy=False, cql_columns=None, *args, **kwargs):
-        super(CassandraFrame, self).__init__(data, index, columns, dtype, copy, *args, **kwargs)
+
+        super(CassandraFrame, self).__init__(data, index=index, columns=columns, dtype=dtype, copy=copy, *args, **kwargs)
 
         self.set_session(session)
-        self.table = table
-        self.cql = kwargs.get('cql', None)
+
+        self.table         = table
+        self.cql           = kwargs.get('cql', None)
+        self.insert_queue  = Queue()
+ 
         self.set_cql_columns(cql_columns)
-        self.insert_queue = Queue()
 
 
     def put(self, table=None):
+        """
+        TODO: (???)
+        """
         if table is not None:
             self.table = table
-
         pass
+
 
     @property
     def create_cql_insert(self):
         """
-        Given a table, prepares a statement to allow the dataframe to be inserted row by row into cassandra
+        Given a table, prepares a statement to allow the dataframe to be inserted row by row into cassandra.
 
-        Sets statement_input to be the prepared statement
+        Sets statement_input to be the prepared statement.
+
         :return: 0
         """
         assert isinstance(self.session, Session)
         assert self.table is not None
 
-        statement = "INSERT INTO " + self.table + "(" + paste(self.cql_columns) + ") VALUES (" + paste(["?"] * len(self.columns)) + ");"
-        self.statement_input = self.session.prepare(statement)
-        self.__prepared_columns__ = self.cql_columns
-        return 0
+        statement = "INSERT INTO " + self.table + "(" + paste(self._cql_columns) + ") VALUES (" + paste(["?"] * len(self.columns)) + ");"
+
+        self.statement_input   = self.session.prepare(statement)
+        self._prepared_columns = self._cql_columns
+
+        return
+
 
     def insert_sync(self):
-        assert self.__cql_columns==self.__prepared_columns__
-        if self.statement_input is None:
-            raise ValueError('statement_input not defined. Use create_cql_insert()')
+        """
+        Insert rows synchronously into Cassandra.
 
-        for index, row in self.loc[:,self.__prepared_columns__].iterrows():
+        Cassandra doesn't get a performance improvement from batch insertion as it is a peer-to-peer architecture;
+        so the insertion strategy is to iterate over the CassandraFrame's rows and bind them one by one.
+        """
+        assert self._cql_columns == self._prepared_columns
+        assert self.statement_input is not None, 'Statement_input not defined. Use create_cql_insert().'
+
+        for index, row in self.loc[:,self._prepared_columns].iterrows():
             self.session.execute(self.statement_input.bind(row))
 
-        return 0
+        return
 
 
     def insert_async(self):
-        assert self.cql_columns==self.__prepared_columns__
-        if self.statement_input is None:
-            raise ValueError('statement_input not defined. Use create_cql_insert()')
+        """
+        Insert rows asynchronously into Cassandra.
+
+        TODO: distinguish from the chained callback approach in insert_callback() and clean up.
+        """
+        assert self._cql_columns == self._prepared_columns
+        assert self.statement_input is not None, 'Statement_input not defined. Use create_cql_insert().'
 
         def handle_success(rows):
             pass
 
         def handle_error(exception):
-            log.error("Failed to send data info: %s", exception)
-            return 0
+            logging.error("Failed to send data info: %s", exception)
+            return
 
         def put(i):
-            future = self.session.execute_async(self.statement_input.bind(self.loc[i, self.__prepared_columns__]))
+            future = self.session.execute_async(self.statement_input.bind(self.loc[i, self._prepared_columns]))
             future.add_callbacks(handle_success, handle_error)
             return future
 
         map(put, range(self.__len__()))
 
-        return 0
+        return
 
 
     def insert_callback(self):
-        assert self.cql_columns==self.__prepared_columns__
-        if self.statement_input is None:
-            raise ValueError('statement_input not defined. Use create_cql_insert()')
+        """
+        TODO: code in the upper limit on concurrent futures, clean up (and deprecate insert_async()?)
+
+        Put row indices into a queue; 
+        while the queue is not empty and the upper threshold on number of concurrent waiting processes is not reached,
+        insert a new row into Cassandra.
+        """
+        assert self._cql_columns == self._prepared_columns
+        assert self.statement_input is not None, 'Statement_input not defined. Use create_cql_insert().'
 
         map(self.insert_queue.put_nowait, range(self.__len__()))
 
-        #for i in range(min(120, self.__len__())):
         def handle_success(rows):
-            i = self.insert_queue.get()
-            print i, "\n", "-----------------------------------------------"
+            """
+            Queue raises an Empty exception when it hits the bottom of the queue (after blocking for `timeout` seconds).
 
-            if i is None:
-                return 0
-            future = self.session.execute_async(self.statement_input.bind(self.loc[i, self.__prepared_columns__]))
-            future.add_callbacks(handle_success, handle_error)
+            Try getting until Queue is exhausted, then return.
+            """
+            try:
+                i = self.insert_queue.get()
+            except Empty:
+                return
 
+            print "Inserting", self.iloc[i].name, " ..." 
+            print "-----------------------------------------------"
+
+            future = self.session.execute_async(self.statement_input.bind(self.loc[i, self._prepared_columns]))
+
+            future.add_callbacks(handle_success, handle_error)  # intentional tail recursion!
+                                                                # need hard upper limit on number of concurrent futures
+                                                                # something like: for i in range(min(120, self.__len__())):
             return future
 
         def handle_error(exception):
-            log.error("Failed to send data info: %s", exception)
+            """
+            Log error and recurse.
+            """
+            logging.error("Failed to send data info: %s", exception)
             future = handle_success(None)
             return future
 
         future = handle_success(None)
+
         return future.result()
-
-    # def insert_next(self, previous_result=sentinel):
-        # if previous_result is not sentinel:
-        #     if isinstance(previous_result, BaseException):
-        #         log.error("Error on insert: %r", previous_result)
-            # if num_finished.next() >= num_queries:
-            #     finished_event.set()
-
-        # if num_started.next() <= num_queries:
-        #     future = session.execute_async(query)
-            # NOTE: this callback also handles errors
-            # future.add_callbacks(insert_next, insert_next)
-
-    # for i in range(min(120, num_queries)):
-    #     insert_next()
-
-    # finished_event.wait()
-
-
 
 
     def get_cql_columns(self):
-        return self.cql_columns
+        return self._cql_columns
+
 
     def set_cql_columns(self, x=None):
         if x is None:
-            self.cql_columns = self.columns.tolist()
+            self._cql_columns = self.columns.tolist()
         else:
             assert isinstance(x, list)
-            self.cql_columns = x
+            self._cql_columns = x
 
-        return 0
+        return
+
 
     def set_session(self, session):
+        """
+        Setter method for self.session.
+
+        Pass a session object or None.
+
+        :return: None
+        """
         if session is None:
             self.session = None
+
         else:
-            assert isinstance(session, Session)
+            assert isinstance(session, Session), "Got non-session, type: {}".format(type(session))
             self.session = session
-        return 0
+
+        return
 
 
-def paste(x, sep = ", "):
-    return str(x).strip("[]").replace("'","").replace(", ", sep)
+
