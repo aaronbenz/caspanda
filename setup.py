e7ebf2935a9a60fc906eabbb65f67a0353b3c6e6
hack-c <charles.hack@accenture.com>
2015-05-19 01:07:32 -0400

    cosmetic edits to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from setuptools import find_packages
 from setuptools import setup
+from setuptools import find_packages
+
+
 setup(
     name='caspanda',
     version='0.0.0.2',
     packages=find_packages(),
-    install_requires=['cassandra-driver',
-              'numpy',
-              'pandas'],
+    install_requires=[
+        'cassandra-driver',
+        'numpy',
+        'pandas',
+    ],
     url='',
     license='MIT',
     author='Aaron Benz',
     author_email='aaron.benz@accenture.com',
     description='Cassandra Wrapper for Easy Panda DataFrame Access',
-    include_package_data=True
+    include_package_data=True,
 )
